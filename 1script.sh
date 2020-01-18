0eb79df9f5860e2d0c337e192dc2f76f42b8d442
offsecdawn <offsecdawn@gmail.com>
2020-01-18 07:02:40 +0000

    Code commit

---
+++ 1script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+if [[ -z $1 ]];
+then
+	echo "Please provide the root domain file name!"
+	exit
+fi
+
+rm -f allfiles.txt uniq_files.txt wayback_only_html.txt wayback_js_files.txt wayback_httprobe_file.txt wayback_json_files.txt
+echo "Currently waybackurls extract is in progress!!"
+for i in $(cat $1)
+do
+	waybackurls $i >> allfiles.txt
+done
+echo "Waybackurls extraction is complete!!"
+sort -ru allfiles.txt >> uniq_files.txt
+echo "Uniq file also created. please check [uniq_files.txt]"
+echo "Now, we need to extract only html files from the list"
+grep -iv -E -- '.js|.png|.jpg|.gif|.ico|.img|.css' uniq_files.txt >> wayback_only_html.txt
+echo "We have extracted all html files.Please check [wayback_only_html.txt]"
+echo "Next is to extracct js files from the list"
+cat uniq_files.txt | grep "\.js" | uniq | sort >> wayback_js_files.txt
+cat uniq_files.txt | grep "\.json" | uniq | sort >> wayback_json_files.txt
+echo "Js files have been successfully extracted [wayback_js_files.txt]"
+echo "Json files have been successfully extracted [wayback_json_files.txt]"
+echo "Process is complete"
+echo "Now start takin screensots selectively"
+echo "The command:"
+echo "--------------"
+echo "cat wayback_only_html.txt | aquatone -threads 20"
+
+
